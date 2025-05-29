29c6dcbff69eb2432b06777ab69b279f6973dbf6
Dan Saleh <dansaleh@WestWallasey.com>
2025-05-29 15:58:50 +0100

    set up test email endpoint

--- dist/server.js
+++ dist/server.js
@@ -13,10 +13,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const koa_1 = __importDefault(require("koa"));
+const koa_router_1 = __importDefault(require("koa-router"));
+const koa_bodyparser_1 = __importDefault(require("koa-bodyparser"));
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = new koa_1.default();
-app.use((ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    ctx.body = "Hello World";
+const router = new koa_router_1.default();
+app.use((0, koa_bodyparser_1.default)());
+router.get("/", (ctx) => {
+    ctx.status = 200;
+    ctx.body = { msg: "hey there!" };
+});
+router.post("/api/email-enquiry", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(process.env.MAIL_TOKEN);
+    var transport = nodemailer_1.default.createTransport({
+        host: "live.smtp.mailtrap.io",
+        port: 587,
+        auth: {
+            user: "api",
+            pass: process.env.MAIL_TOKEN,
+        },
+    });
+    transport
+        .sendMail({
+        from: "info@demomailtrap.co",
+        to: process.env.EMAIL,
+        subject: "You are awesome!",
+        text: "Congrats for sending test email with Mailtrap!",
+    })
+        .then(console.log, console.error);
 }));
+//you have to register the routing middleware
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.listen(3000, () => {
     console.log("Server listening on PORT 3000...");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,46 @@
 import Koa from "koa";
+import Router from "koa-router";
+import bodyParser from "koa-bodyparser";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World";
+app.use(bodyParser());
+
+router.get("/", (ctx) => {
+  ctx.status = 200;
+  ctx.body = { msg: "hey there!" };
 });
 
+router.post("/api/email-enquiry", async (ctx) => {
+  console.log(process.env.MAIL_TOKEN);
+  var transport = nodemailer.createTransport({
+    host: "live.smtp.mailtrap.io",
+    port: 587,
+    auth: {
+      user: "api",
+      pass: process.env.MAIL_TOKEN,
+    },
+  });
+
+  transport
+    .sendMail({
+      from: "info@demomailtrap.co",
+      to: process.env.EMAIL,
+      subject: "You are awesome!",
+      text: "Congrats for sending test email with Mailtrap!",
+    })
+    .then(console.log, console.error);
+});
+
+//you have to register the routing middleware
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 app.listen(3000, () => {
-    console.log("Server listening on PORT 3000...")
+  console.log("Server listening on PORT 3000...");
 });
